72876677e9ef814a25770f4599c735ec6074851b
Lio <liomazza@gmail.com>
2021-05-14 09:40:16 -0300

    add recyclerview

--- app/src/main/java/com/example/lio/todolist/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/example/lio/todolist/ui/tasks/TasksViewModel.kt
@@ -2,10 +2,13 @@ package com.example.lio.todolist.ui.tasks
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.example.lio.todolist.data.TaskDao
 
 
 class TasksViewModel @ViewModelInject constructor(
     private val taskDao: TaskDao
 ): ViewModel() {
+
+    val tasks = taskDao.getTasks().asLiveData()
 }

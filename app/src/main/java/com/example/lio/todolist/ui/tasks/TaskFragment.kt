ab6e60841a2ad5313cbda00c9fd72717e3f3c111
Lio <liomazza@gmail.com>
2021-05-15 17:53:50 -0300

    search task feature

--- app/src/main/java/com/example/lio/todolist/ui/tasks/TaskFragment.kt
+++ app/src/main/java/com/example/lio/todolist/ui/tasks/TaskFragment.kt
@@ -1,13 +1,18 @@
 package com.example.lio.todolist.ui.tasks
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.lio.todolist.R
 import com.example.lio.todolist.databinding.FragmentTasksBinding
+import com.example.lio.todolist.util.onQueryTextChanged
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -31,6 +36,38 @@ class TaskFragment: Fragment(R.layout.fragment_tasks) {
         viewModel.tasks.observe(viewLifecycleOwner, Observer {
             taskAdapter.submitList(it)
         })
+        setHasOptionsMenu(true)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_fragment_tasks, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.onQueryTextChanged {
+            // update search query
+            viewModel.searchQuery.value = it
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+       return when(item.itemId) {
+           R.id.action_sort_by_name -> {
+              true
+           }
+           R.id.action_sort_by_date_created -> {
+               true
+           }
+           R.id.action_hide_completed_tasks -> {
+               item.isChecked = !item.isChecked
+
+               true
+           }
+           R.id.action_delete_all_completed_tasks -> {
+               true
+           }
+           else -> super.onOptionsItemSelected(item)
+       }
     }
 }
---
+++ app/src/main/java/com/example/lio/todolist/util/ViewExt.kt
@@ -0,0 +1,17 @@
+package com.example.lio.todolist.util
+
+import android.widget.SearchView
+
+inline fun SearchView.onQueryTextChanged(crossinline listener: (String) -> Unit) {
+    this.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+        override fun onQueryTextSubmit(query: String?): Boolean {
+            return true
+        }
+
+        override fun onQueryTextChange(newText: String?): Boolean {
+           listener(newText.orEmpty())
+            return true
+        }
+
+    })
+}
